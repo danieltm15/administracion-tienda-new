dde4c61540b8f665af14529903ea342a09013bca
danieltm15 <danieltoledo@gmail.com>
2022-08-11 23:17:27 -0500

    error

--- servidor.py
+++ servidor.py
@@ -226,6 +226,7 @@ def del_fact():
     requestdata=request.form
     id_factura=requestdata["id_factura"]
     facturadb=Factura.query.filter_by(id_factura=id_factura).first()
+    db.session.delete(facturadb)
     return redirect("elimfact")
 
 
